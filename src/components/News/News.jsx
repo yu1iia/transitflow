8c26ec4ea9937ee02978d3e15eb1796e05387652
Yuliia Burko <yuliia.burko@gmail.com>
2024-04-29 21:42:06 +0200

    added news section

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -2,6 +2,10 @@ import styles from './News.module.css';
 import Image from 'next/image';
 
 import line from '../../images/pattern1.png';
+import cargo from '../../images/cargo7.png';
+import moving from '../../images/moving6.png';
+import barge from '../../images/cargo6.png';
+import calendarIcon from '../../images/calendar-icon.png';
 
 const News = () => {
   return (
@@ -19,6 +23,128 @@ const News = () => {
         </div>
         <h2 className={styles.header}>Our Latest News</h2>
       </div>
+      <ul className={styles.newsList}>
+        <li className={styles.newsElement}>
+          <Image
+            className={styles.newsImage}
+            src={cargo}
+            alt="cargo"
+            width={453}
+            height={308}
+          />
+          <span className={styles.newsDate}>
+            <Image
+              className={styles.calendarIcon}
+              src={calendarIcon}
+              alt="calendar icon"
+              width={56}
+              height={52}
+            />
+            <span className={styles.newsDay}>8</span>
+            <span className={styles.newsMonth}>September</span>
+          </span>
+          <div className={styles.newsDescription}>
+            <h3 className={styles.newsTitle}>
+              Inland freight a worthy solution for your business
+            </h3>
+            <p className={styles.newsContent}>
+              We are dedicated in creating added value for our customers by
+              implementing modern technology in our work.
+            </p>
+            <ul className={styles.newsContentList}>
+              <li className={styles.newsDetails}>Urgent transport solutions</li>
+              <li className={styles.newsDetails}>
+                Reliable & experienced staffs
+              </li>
+              <li className={styles.newsDetails}>Urgent transport solutions</li>
+              <li className={styles.newsDetails}>
+                Reliable & experienced staffs
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li className={styles.newsElement}>
+          <Image
+            className={styles.newsImage}
+            src={moving}
+            alt="boxes"
+            width={453}
+            height={308}
+          />
+          <span className={styles.newsDate}>
+            <Image
+              className={styles.calendarIcon}
+              src={calendarIcon}
+              alt="calendar icon"
+              width={56}
+              height={52}
+            />
+            <span className={styles.newsDay}>12</span>
+            <span className={styles.newsMonth}>September</span>
+          </span>
+          <div className={styles.newsDescription}>
+            <h3 className={styles.newsTitle}>
+              How technology can help redraw the supply chain map
+            </h3>
+            <p className={styles.newsContent}>
+              We are dedicated in creating added value for our customers by
+              implementing modern technology in our work.
+            </p>
+            <ul className={styles.newsContentList}>
+              <li className={styles.newsDetails}>Urgent transport solutions</li>
+              <li className={styles.newsDetails}>
+                Reliable & experienced staffs
+              </li>
+              <li className={styles.newsDetails}>Urgent transport solutions</li>
+              <li className={styles.newsDetails}>
+                Reliable & experienced staffs
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li className={styles.newsElement}>
+          <Image
+            className={styles.newsImage}
+            src={barge}
+            alt="barge"
+            width={453}
+            height={308}
+          />
+          <span className={styles.newsDate}>
+            <Image
+              className={styles.calendarIcon}
+              src={calendarIcon}
+              alt="calendar icon"
+              width={56}
+              height={52}
+            />
+            <span className={styles.newsDay}>25</span>
+            <span className={styles.newsMonth}>September</span>
+          </span>
+          <div className={styles.newsDescription}>
+            <h3 className={styles.newsTitle}>
+              Five things you should have ready for your broker
+            </h3>
+            <p className={styles.newsContent}>
+              We are dedicated in creating added value for our customers by
+              implementing modern technology in our work.
+            </p>
+            <ul className={styles.newsContentList}>
+              <li className={styles.newsDetails}>Urgent transport solutions</li>
+              <li className={styles.newsDetails}>
+                Reliable & experienced staffs
+              </li>
+              <li className={styles.newsDetails}>Urgent transport solutions</li>
+              <li className={styles.newsDetails}>
+                Reliable & experienced staffs
+              </li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+      <button type="button" className={styles.transportingMoreButton}>
+        More Blog
+      </button>
     </div>
   );
 };
